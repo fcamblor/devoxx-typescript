033edc17060f600f30ee6f015f01bf686ee48b69
fcamblor <fcamblor@gmail.com>
2016-09-30 15:56:29 +0200

    Skeleton generation

---
+++ ui/app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('4sh-workshops-pollApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'RESTX',
+      'Twitter Bootstrap',
+      'AngularJS',
+      'Karma'
+    ];
+  });
