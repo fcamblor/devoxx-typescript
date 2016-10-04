92672a8ddd8463e97f18bd87e254af3e14cadcd0
fcamblor <fcamblor@gmail.com>
2016-10-04 20:10:35 +0200

    Making Views.* classes inherit from Public View

--- srv/src/main/java/fr/fsh/rest/Views.java
+++ srv/src/main/java/fr/fsh/rest/Views.java
@@ -4,10 +4,9 @@ package fr.fsh.rest;
  * Created by fcamblor on 30/09/16.
  */
 public class Views extends restx.jackson.Views {
-    public interface Listing {
+    public class Listing extends restx.jackson.Views.Public {
     }
 
-    public interface Detail {
+    public class Detail extends restx.jackson.Views.Public {
     }
-
 }
