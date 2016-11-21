177c17c81bb53d13d6b552fed0271d106b8cb99c
fcamblor <fcamblor@gmail.com>
2016-11-21 02:00:40 +0100

    Updated global layout and provided pageTitle rootScope variable

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -6,19 +6,16 @@ angular.module('4sh-workshops-pollApp', [
   'ngSanitize',
   'ui.router'
 ])
+.run(['$rootScope', '$transitions', function($rootScope, $transitions) {
+  $transitions.onSuccess({}, function(transition){
+    $rootScope.pageTitle = transition.targetState()._definition.title || "TODO: PROVIDE TITLE IN STATE";
+  });
+}])
 .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
   $urlRouterProvider.otherwise('/workshops');
 
   $stateProvider
-    .state({
-      name: 'app',
-      abstract: true,
-      template: "<ui-view/>"
-    })
-    .state({
-      name: 'app.workshops',
-      url: '/workshops',
-      component: 'workshopListPage'
-    });
+    .state({ name: 'app', abstract: true, template: "<ui-view/>" })
+    .state({ name: 'app.workshops', url: '/workshops', component: 'workshopListPage', title: "Workshops" });
 });
