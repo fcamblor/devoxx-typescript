fedcae1dc94b802129b5660105d91f22ca48cf6a
fcamblor <fcamblor@gmail.com>
2016-11-22 01:51:48 +0100

    Improved workshop votes screen

--- ui/app/scripts/controllers/workshopVotePage.js
+++ ui/app/scripts/controllers/workshopVotePage.js
@@ -10,6 +10,19 @@ angular.module('4sh-workshops-pollApp')
       var self = this;
       console.log($stateParams.workshopId);
       angular.extend(self, {
+        voterName: "",
+        votesPerTopic: _.reduce(self.detailedPoll.topics, function(result, topic) {
+          result[topic.title] = 0;
+          return result;
+        }, {}),
+        remainingVotePoints: function(){
+          return 10 - _.reduce(self.votesPerTopic, function(result, score, topic) {
+            return result + Number(score);
+          }, 0);
+        },
+        isValid: function(){
+          return self.remainingVotePoints()>=0 && self.voterName && self.voterName.length >= 3;
+        }
       });
     }
   });
