08086e318c5361a1c9fa10ddbbb3e54100ac4ec0
fcamblor <fcamblor@gmail.com>
2017-03-23 21:20:52 +0100

    Migrated WorkshopListPage to typescript

---
+++ ui/app/scripts/controllers/workshopListPage.ts
@@ -0,0 +1,36 @@
+
+
+class WorkshopListController {
+  private polls: any[] = [];
+
+  public static $inject = ['$http', 'WorkshopLogger'];
+  constructor($http, WorkshopLogger){
+    $http.get("/api/polls").then((polls) => {
+      this.polls.length = 0;
+      Array.prototype.push.apply(this.polls, polls.data);
+    }, (error) => WorkshopLogger.error(error));
+  }
+}
+
+
+const WORKSHOP_LIST_PAGE = {
+  template: `
+<div class="mdl-grid">
+  <div ng-repeat="poll in $ctrl.polls" class="mdl-cell mdl-cell--6-col mdl-cell--12-col-phone mdl-shadow--4dp">
+    <div class="mdl-card__supporting-text">
+      <h4>{{poll.name}}</h4>
+      <button ui-sref="app.workshops-vote({ workshopId: poll._id })" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+        Vote for topics
+      </button>
+    </div>
+  </div>
+</div>
+  `,
+  bindings: {
+    polls: '<'
+  },
+  controller: WorkshopListController
+};
+
+angular.module('4sh-workshops-pollApp')
+  .component('workshopListPage', WORKSHOP_LIST_PAGE);
